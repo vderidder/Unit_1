d5df285f91f906b0c34405dd4d90e8d35e698cc9
Ridney7277 <victor.deridder2006@gmail.com>
2022-09-07 17:25:51 +0200

    Added comments

--- src/BinaryToFromAscii.java
+++ src/BinaryToFromAscii.java
@@ -29,33 +29,33 @@ public class BinaryToFromAscii
         }
         else if (choice == 3)
         {
-            System.out.println("Why?");
+            System.out.println("Why?"); // Hey robots have feelings too...
         }
     }
 
     private static void binaryToText()
     {
-        StringBuilder textOutput = new StringBuilder();
+        StringBuilder textOutput = new StringBuilder(); // Framework for the output
+        // I'll be honest I've never used "String builder" IntelLiJ just suggested it when I was building a string
         System.out.println("Enter Binary: ");
-        String SneakySpaceIncoming = input.nextLine();
-        int u = 0;
-        String binary = SneakySpaceIncoming + " ";
-
+        String SneakySpaceIncoming = input.nextLine(); // Input for the binary code
+        int u = 0; // used later
+        String binary = SneakySpaceIncoming + " "; // adding a space at the end to make it even for the loop
         for(int i = 0; i < (binary.length()/9); i++)
         {
-            int unicodeIndex = 0;
+            int unicodeIndex = 0; // Used to find the letter described in binary
             for(int w = 128; w > 0; w /= 2)
             {
-                String binarySnippet = binary.substring(u, u+1);
+                String binarySnippet = binary.substring(u, u+1); // Gets each number individually
                 if(binarySnippet.equals("1"))
                 {
-                    unicodeIndex += w;
+                    unicodeIndex += w; // adds the value if it is present
                 }
-                u++;
+                u++; // Along with the for loop parameter, it will run exactly 8 times
             }
-            u++;
-            char letter = (char) unicodeIndex;
-            textOutput.append(letter);
+            u++; // Used to skip the space
+            char letter = (char) unicodeIndex; // transforms the unicode to a letter or symbol
+            textOutput.append(letter); // adds the letter to the final string
         }
         System.out.println(textOutput);
         run();
@@ -65,26 +65,26 @@ public class BinaryToFromAscii
     private static void textToBinary()
     {
         System.out.println("Enter Text: ");
-        String text = input.nextLine();
+        String text = input.nextLine(); // input for text
 
-        StringBuilder binaryOutput = new StringBuilder();
+        StringBuilder binaryOutput = new StringBuilder(); // Once again, I'm not sure why, but it was suggested
         for(int i = 0; i < text.length(); i++)
         {
-            int asciiValue = text.charAt(i);
+            int asciiValue = text.charAt(i); // grabbing 1 letter as a char and finding its unicode value
 
-            for(int e = 128; e > 0; e /= 2)
+            for(int e = 128; e > 0; e /= 2) // for loop to go through all binary possibilities 128 64 etc...
             {
-                if((asciiValue - e) >= 0)
+                if((asciiValue - e) >= 0) // checks if the unicode value can be subtracted by the respective binary one
                 {
-                    binaryOutput.append("1");
+                    binaryOutput.append("1"); // the unicode value can be subtracted
                     asciiValue -= e;
                 }
                 else
                 {
-                    binaryOutput.append("0");
+                    binaryOutput.append("0"); // the unicode value cannot
                 }
             }
-            binaryOutput.append(" ");
+            binaryOutput.append(" "); // Putting a space to space out the 8 digits
         }
         System.out.println(binaryOutput);
         run();
